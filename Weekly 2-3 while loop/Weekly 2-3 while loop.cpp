f50da1266fbce414e6c50e311473e9989ab3d971
JanOveBicepS <olemr16@gmail.com>
2020-09-14 16:51:07 +0200

    Add project files.

---
+++ Weekly 2-3 while loop/Weekly 2-3 while loop.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+
+
+int main() {
+	int i = 1;
+	
+	while (i <= 50 )
+	{
+		if (i=i+4)
+		{
+			std::cout << i << " ";
+			
+		}
+		
+		i++;
+		;
+ 	}
+
+
+
+
+}
